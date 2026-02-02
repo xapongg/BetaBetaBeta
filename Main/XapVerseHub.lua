dc04d764a700f5d499d1ed336801a7daad3974ad
xapongg <xaponggg@gmail.com>
2026-02-02 13:33:58 +0700

    Update XapVerseHub.lua

--- Main/XapVerseHub.lua
+++ Main/XapVerseHub.lua
@@ -2,7 +2,6 @@ local games = {
     [79657240466394] = "https://raw.githubusercontent.com/xapongg/XapVerseHub/refs/heads/main/ContainerRNG/main.lua",
     [82013336390273] = "https://raw.githubusercontent.com/xapongg/XapVerseHub/refs/heads/main/PickaxeSim/main.lua",
     [136404558442020] = "https://raw.githubusercontent.com/xapongg/XapVerseHub/refs/heads/main/Kayak%20And%20Surf/main.lua",
-    [] = "",
 }
 
 local currentID = game.PlaceId
